1bd53ca1ecaf2aa6b9e17a2c1b004a70f78455c3
Niklas Ladehoff <business-mail-niklas@web.de>
2023-12-21 18:24:15 +0100

    feat(backend): add user dependency

--- backend/routers/auth/auth.py
+++ backend/routers/auth/auth.py
@@ -23,6 +23,8 @@ bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 oauth_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
 
+
+
 def authenticate_user(username: str, password: str, db: type[Session]):
     user: Users = db.query(Users).filter(Users.username == username).first()
 
@@ -40,6 +42,23 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta):
     encode.update({"exp": expires})
 
     return jwt.encode(encode, os.getenv("SECRET_KEY"), algorithm=ALGORITHM)
+
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth_bearer)]):
+    try:
+        payload = jwt.decode(token, os.getenv("SECRET_KEY"), algorithms=ALGORITHM)
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
+
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user.")
+        return {"username": username, "id": user_id}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user.")
+    
+    
+user_dependency = Annotated[dict, Depends(get_current_user)]
     
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
@@ -57,7 +76,7 @@ async def login_for_access_token(db: db_dependency, form_data: Annotated[OAuth2P
     user = authenticate_user(form_data.username, form_data.password, db)
 
     if not user:
-        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user.")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user.")
 
     token = create_access_token(user.username, user.id, timedelta(days=3))
 
