233610cff2a69dd5e079693c43969c2e95b81aad
Niklas Ladehoff <business-mail-niklas@web.de>
2023-12-23 16:36:18 +0100

    chore(backend): add IDE settings

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,25 +10,22 @@ from routers.auth.services import user_dependency
 from routers.auth import models as auth_models
 from routers.auth.auth import router as auth_router
 
-
 load_dotenv()
 
 app = FastAPI()
 
 app.include_router(auth_router)
 
-
 auth_models.Base.metadata.create_all(bind=engine)
 
 
-
-
 @app.get("/", status_code=status.HTTP_200_OK)
-async def user(user: user_dependency, db:db_dependency):
+async def user(user: user_dependency, db: db_dependency):
     if user is None:
         raise HTTPException(status_code=401, detail="Authentication failed.")
-    
+
     return {"user": user}
+
 
 @app.get("/test", status_code=status.HTTP_200_OK)
 async def test():
