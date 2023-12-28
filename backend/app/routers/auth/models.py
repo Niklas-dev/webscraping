c124981a4d3d41f858eacb47b225ddcb1c8470ba
Niklas Ladehoff <business-mail-niklas@web.de>
2023-12-28 01:56:17 +0100

    fix(backend): change google_id from being numeric to varchar

--- backend/app/routers/auth/models.py
+++ backend/app/routers/auth/models.py
@@ -1,11 +1,11 @@
-from ...db.database import Base
+from db.database import Base
 from sqlalchemy import Column, Integer, String
 
 class Users(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    google_id = Column(Integer)
+    google_id = Column(String, unique=True, nullable=True)
     username = Column(String, unique=True)
     email = Column(String, unique=True)
     hashed_password = Column(String)
